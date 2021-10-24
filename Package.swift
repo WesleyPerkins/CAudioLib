ccc07cd4daede3a7f2d85b3014330d682065ace0
Wesley Perkins <wesley@wesleyperkins.org>
2021-10-24 17:22:37 +0700

    not sure

--- Package.swift
+++ Package.swift
@@ -21,8 +21,5 @@ let package = Package(
         .target(
             name: "CAudioLib",
             dependencies: []),
-        .testTarget(
-            name: "CAudioLibTests",
-            dependencies: ["CAudioLib"]),
     ]
 )
--- Sources/CAudioLib/CAudioLib.swift
+++
@@ -1,6 +0,0 @@
-public struct CAudioLib {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
